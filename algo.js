4cf43f59f6cdee87fa3d3b5a35c34c3599a383b4
Иванович Иванов <explay167@gmail.com>
2023-03-21 13:45:11 +0300

    opposite

--- algo.js
+++ algo.js
@@ -1,19 +1,29 @@
 function makeNegative(num) {
-  if(num <= 0){
+  if (num <= 0) {
     return num
-  }else{
-    return num  - num * 2
+  } else {
+    return num - num * 2
   }
 }
 
 
-function solution(str){
+function solution(str) {
   return [...str].reverse().join('')
 }
 
-const boolToWord = ( bool ) => bool ? "Yes" : "No"
+const boolToWord = (bool) => bool ? "Yes" : "No"
 
 
 function numberToString(num) {
   return num.toString()
+}
+
+function opposite(number) {
+  if (number < 0) {
+    return number - (number * 2)
+  } else if (number === 0) {
+    return number
+  } else {
+    return number - (number * 2)
+  }
 }
