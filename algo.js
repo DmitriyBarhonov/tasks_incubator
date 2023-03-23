02477160314469fdd2fc9caa4e649cefb5b753a4
Иванович Иванов <explay167@gmail.com>
2023-03-23 18:38:14 +0300

    mallestntegerinder

--- algo.js
+++ algo.js
@@ -64,3 +64,15 @@ const  summation = (num)=> {
 function noSpace(x){
   return x.replace(/ /g,'')
 }
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let min = Infinity
+    for(let i = 0; i < args.length; i++){
+      if (args[i] < min){
+        min = args[i]
+      }
+    }
+  return min
+  }
+}
