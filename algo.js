f89b7c5a165d0a1770fa35d377458234bd7755a1
Иванович Иванов <explay167@gmail.com>
2023-04-17 17:43:15 +0300

    countSheeps

--- algo.js
+++ algo.js
@@ -138,4 +138,13 @@ function makeUpperCase(str) {
     return false
   }
   }
+  function countSheeps(arr) {
+    let count = 0
+     for(let i = 0; i < arr.length; i++){
+       if(arr[i] === true){
+         count ++
+       }
+     }
+    return count
+  }
   
